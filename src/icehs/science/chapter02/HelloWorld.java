423b4487a42dc324912ac2feaab2f28d31139bf8
학생51 <51@DESKTOP-6N93BJ7>
2019-05-05 14:15:20 +0900

    Chapter 2

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,11 +1,16 @@
 package icehs.science.chapter02;
+ 
+/*
+ * my first java classssssssssssssssssssssssssssssssssssssssssssssss
+ */
 
 public class HelloWorld {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("helloWorld"); 
-		System.out.println("¾Ó");
+		System.out.println("¾Ó");// Á¶¼º¹Î ¸¶ºıÀÌ ÅÂÆò¾ç
+		
 	}
 
 }
